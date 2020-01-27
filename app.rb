eb6a686de426d10f6b6b81e5168bb7f43b42363b
NgoHungDong <dongngo0804@gmail.com>
2020-01-27 14:48:47 +0700

    Change questions to professional

--- app.rb
+++ app.rb
@@ -4,9 +4,27 @@ Bundler.require
 set :questions, JSON.parse(File.read('./final.json'))
 
 get '/' do
+  erb :index
+end
+
+get '/random' do
   questions = settings.questions
-  @random_chapter = (1..18).to_a.sample
+  @random_chapter = (1..12).to_a.sample
   @random_question = questions[@random_chapter.to_s].sample
   @correct_index = @random_question['answer'].match(/\d+\.\s+(\w)/).captures.first.upcase.ord - "A".ord + 1
-  erb :index
+  erb :random
+end
+
+get '/full' do
+  erb :full
+end
+
+get '/by_chapter' do
+  @questions = settings.questions
+  erb :by_chapter
+end
+
+get '/by_chapter/:id' do
+  @questions = settings.questions[params[:id]]
+  erb :chapter
 end
